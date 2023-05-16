f3c7fd521325883328ded410a29e8f789fea51ff
--v <huutai507@gmail.com>
2023-05-16 17:06:16 +0700

    update

--- data/data.js
+++ data/data.js
@@ -1,5 +1,14 @@
 export const data = [
   {
+    name: '753',
+    podlink: 'https://www.alenascarlets.com/collections/create-your-own-custom-art',
+    usalink: '',
+    aliexpress: '',
+    imglink: '//ae01.alicdn.com/kf/S70738568f16e4e19b4ce4a025747ab0b7.jpg_350x350.jpg',
+    shortlink: '',
+    customlink: 'https://www.alenascarlets.com/products/car-sunshade-umbrella'
+  },
+  {
     name: '752',
     podlink: 'https://www.alenascarlets.com/collections/create-your-own-custom-art',
     usalink: '',
