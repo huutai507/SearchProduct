cd594475989cc20d6cbd1aacce13da8be37933a9
--v <huutai507@gmail.com>
2023-05-23 15:42:32 +0700

    update

--- data/data.js
+++ data/data.js
@@ -367,7 +367,7 @@ export const data = [
     aliexpress: '',
     imglink: '//ae01.alicdn.com/kf/S7e87e2cc0c6447a089e7f23cfffef04b2.jpg_350x350.jpg',
     shortlink: '',
-    customlink: 'https://www.alenascarlets.com/products/shower-for-bathroom-toilet'
+    customlink: 'https://www.alenascarlets.com/products/out-dual-control-valve'
   },
   {
     name: '728',
