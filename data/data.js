1638048975369813d124cff935ca1eb404cdb7fa
--v <huutai507@gmail.com>
2023-11-21 22:55:13 +0700

    update

--- data/data.js
+++ data/data.js
@@ -9,6 +9,15 @@ export const data = [
     customlink: 'https://hutaw.com/products/wireless-mitter-division-artifact'
   },
   {
+    name: '19',
+    podlink: '',
+    usalink: '',
+    aliexpress: '',
+    imglink: '//ae01.alicdn.com/kf/S2dea4a8989b244e8909a8126face46f8W.jpg_350x350.jpg',
+    shortlink: '',
+    customlink: 'https://hutaw.com/products/rehabilitation-robot-glove-hand'
+  },
+  {
     name: '60',
     podlink: '',
     usalink: '',
