2e8ce9624b8e2a510fddfe2360fa8430b24abcb6
--v <huutai507@gmail.com>
2023-11-30 18:29:58 +0700

    update

--- data/data.js
+++ data/data.js
@@ -18,6 +18,15 @@ export const data = [
     customlink: 'https://hutaw.com/products/electric-rechargeable-lint-remover'
   },
   {
+    name: '66',
+    podlink: '',
+    usalink: '',
+    aliexpress: '',
+    imglink: '//ae01.alicdn.com/kf/S2dea4a8989b244e8909a8126face46f8W.jpg_350x350.jpg',
+    shortlink: '',
+    customlink: 'https://hutaw.com/products/rehabilitation-robot-glove-hand'
+  },
+  {
     name: '65',
     podlink: '',
     usalink: '',
