733af46e33b255329203b688e12e3465b94a97b6
--v <huutai507@gmail.com>
2023-12-19 16:35:40 +0700

    update

--- data/data.js
+++ data/data.js
@@ -1,5 +1,14 @@
 export const data = [
   {
+    name: '88',
+    podlink: '',
+    usalink: '',
+    aliexpress: '',
+    imglink: '//ae01.alicdn.com/kf/Sfdac0d04cb544b53a412001abfa9154bI.jpg_350x350.jpg',
+    shortlink: '',
+    customlink: 'https://hutaw.com/products/silicone-faucet-splash-pad-kitchen-foldable'
+  },
+  {
     name: '87',
     podlink: '',
     usalink: '',
