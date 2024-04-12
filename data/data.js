2ae93ab0d857108ebad6918c312f00433b808643
--v <huutai507@gmail.com>
2024-04-12 15:13:30 +0700

    udate

--- data/data.js
+++ data/data.js
@@ -1,5 +1,14 @@
 export const data = [
   {
+    name: '180',
+    podlink: '',
+    usalink: '',
+    aliexpress: '',
+    imglink: '//ae01.alicdn.com/kf/S16d274e854ff416a87c47cc5567c9f2cP.jpg_350x350.jpg',
+    shortlink: '',
+    customlink: 'https://hutaw.com/products/new-36mm-wavy-hair-curlers'
+  },
+  {
     name: '179',
     podlink: '',
     usalink: '',
