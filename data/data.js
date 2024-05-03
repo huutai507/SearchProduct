9c829735f326588d56f057a254e143b1ee8d1067
--v <huutai507@gmail.com>
2024-05-03 20:50:48 +0700

    update

--- data/data.js
+++ data/data.js
@@ -1,5 +1,14 @@
 export const data = [
   {
+    name: '200',
+    podlink: '',
+    usalink: '',
+    aliexpress: '',
+    imglink: '//ae01.alicdn.com/kf/S2b610dac157f415da93a033d9df5d343F.jpg_350x350.jpg',
+    shortlink: '',
+    customlink: 'https://hutaw.com/products/reuable-hair-removal'
+  },
+  {
     name: '199',
     podlink: '',
     usalink: '',
