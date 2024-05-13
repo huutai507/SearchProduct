b90601452b9d923e7cecf0cada3bfb2a85485224
--v <huutai507@gmail.com>
2024-05-13 19:24:27 +0700

    update

--- data/data.js
+++ data/data.js
@@ -1,5 +1,14 @@
 export const data = [
   {
+    name: '213',
+    podlink: '',
+    usalink: '',
+    aliexpress: '',
+    imglink: '//ae01.alicdn.com/kf/Sd1086d78d27245d0b152a7b89b6f2d12L.jpg_350x350.jpg',
+    shortlink: '',
+    customlink: 'https://hutaw.com/products/electric-rechargeable-lint-remover'
+  },
+  {
     name: '212',
     podlink: '',
     usalink: '',
