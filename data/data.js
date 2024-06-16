3f63444bab96dba7c380196e1c197b50b8117238
Whutai <taiphantmt@gmail.com>
2024-06-16 10:28:12 +0700

    update

--- data/data.js
+++ data/data.js
@@ -1,5 +1,15 @@
 export const data = [
   {
+    name: "256",
+    podlink: "",
+    usalink: "",
+    aliexpress: "",
+    imglink:
+      "//ae01.alicdn.com/kf/S16d274e854ff416a87c47cc5567c9f2cP.jpg_350x350.jpg",
+    shortlink: "",
+    customlink: "https://hutaw.com/products/new-36mm-wavy-hair-curlers"
+  },
+  {
     name: "255",
     podlink: "",
     usalink: "",
