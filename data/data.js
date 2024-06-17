043fbdc13f5fed29a212caa4ce3622e96b5cf441
Whutai <taiphantmt@gmail.com>
2024-06-17 21:12:41 +0700

    update

--- data/data.js
+++ data/data.js
@@ -1,5 +1,15 @@
 export const data = [
   {
+    name: "258",
+    podlink: "",
+    usalink: "",
+    aliexpress: "",
+    imglink:
+      "//ae01.alicdn.com/kf/S54792bba273b419bb77851d291841f2b5.jpg_350x350.jpg",
+    shortlink: "",
+    customlink: "https://hutaw.com/products/wooden-essential-oil-aromatherapy"
+  },
+  {
     name: "257",
     podlink: "",
     usalink: "",
