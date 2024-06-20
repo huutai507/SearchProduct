b419fd6ca7aaf55b2e5b33479e02acf85a965521
Whutai <taiphantmt@gmail.com>
2024-06-20 15:44:11 +0700

    update

--- data/data.js
+++ data/data.js
@@ -1,5 +1,15 @@
 export const data = [
   {
+    name: "261",
+    podlink: "",
+    usalink: "",
+    aliexpress: "",
+    imglink:
+      "//ae01.alicdn.com/kf/Sb82af73275b84a1ebaed1d48d3bf2111s.jpg_350x350.jpg",
+    shortlink: "",
+    customlink: "https://hutaw.com/products/travel-toothbrush-protector-cap"
+  },
+  {
     name: "260",
     podlink: "",
     usalink: "",
