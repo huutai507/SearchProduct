0d66843be1d90c533ea24ec393a08c73aa935bb0
Whutai <taiphantmt@gmail.com>
2024-06-22 19:23:11 +0700

    update

--- data/data.js
+++ data/data.js
@@ -1,5 +1,15 @@
 export const data = [
   {
+    name: "263",
+    podlink: "",
+    usalink: "",
+    aliexpress: "",
+    imglink:
+      "//ae01.alicdn.com/kf/S607278a28b884a80ba37e8efff6d04840.jpg_350x350.jpg",
+    shortlink: "",
+    customlink: "https://hutaw.com/products/bathroom-suction-cup-storage-rack"
+  },
+  {
     name: "262",
     podlink: "",
     usalink: "",
