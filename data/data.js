a30694bdca6fcea13245639a01d92474520ebd13
Whutai <taiphantmt@gmail.com>
2024-06-24 10:32:21 +0700

    update

--- data/data.js
+++ data/data.js
@@ -1,5 +1,15 @@
 export const data = [
   {
+    name: "265",
+    podlink: "",
+    usalink: "",
+    aliexpress: "",
+    imglink:
+      "//ae01.alicdn.com/kf/S60b69b4948ad434583e5c649f55e2d47a.jpg_350x350.jpg",
+    shortlink: "",
+    customlink: "https://hutaw.com/products/wall-mount-pot-lids-holder"
+  },
+  {
     name: "264",
     podlink: "",
     usalink: "",
