ee3737c066884a9d2d1898c95cf941925f5c241c
Whutai <taiphantmt@gmail.com>
2024-07-05 15:26:16 +0700

    update

--- data/data.js
+++ data/data.js
@@ -15,9 +15,10 @@ export const data = [
     podlink: "",
     usalink: "",
     aliexpress: "",
-    imglink: "",
+    imglink:
+      "https://ae01.alicdn.com/kf/S55e8f3e4dd7b4315b115833a3e309f390/Phomemo-M08F-A4-Portable-Thermal-Printer-Supports-8-26-x11-69-A4-Thermal-Paper-Wireless-Mobile.jpg_350x350.jpg_.webp",
     shortlink: "",
-    gif: "printer.webp",
+    gif: "",
     customlink: "https://hutaw.com/products/portable-printer-wireless"
   },
   {
