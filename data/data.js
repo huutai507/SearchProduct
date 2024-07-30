840f0b4c8b55ac250d861f5c013271cfd76a416d
Whutai <taiphantmt@gmail.com>
2024-07-30 10:30:09 +0700

    update

--- data/data.js
+++ data/data.js
@@ -1,5 +1,15 @@
 export const data = [
   {
+    name: "314",
+    podlink: "",
+    usalink: "",
+    aliexpress: "",
+    imglink:
+      "//ae01.alicdn.com/kf/Sd1086d78d27245d0b152a7b89b6f2d12L.jpg_350x350.jpg",
+    shortlink: "",
+    customlink: "https://hutaw.com/products/electric-rechargeable-lint-remover"
+  },
+  {
     name: "313",
     podlink: "",
     usalink: "",
