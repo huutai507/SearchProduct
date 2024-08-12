2a9ca8fffc930f833d3ad7ff8e31d115191eaeb6
Whutai <taiphantmt@gmail.com>
2024-08-12 22:22:30 +0700

    udpate

--- data/data.js
+++ data/data.js
@@ -1,5 +1,15 @@
 export const data = [
   {
+    name: "335",
+    podlink: "",
+    usalink: "",
+    aliexpress: "",
+    imglink:
+      "//ae01.alicdn.com/kf/Se60bac2d087e4ea2911cd368ed1992c6G.jpg_350x350.jpg",
+    shortlink: "",
+    customlink: "https://hutaw.com/products/home-cinema-mini-projetor"
+  },
+  {
     name: "334",
     podlink: "",
     usalink: "",
