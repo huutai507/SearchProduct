bd1b99bc3e551ae68e4a5e239a3a46fe9867a354
Whutai <taiphantmt@gmail.com>
2024-08-13 15:35:38 +0700

    update

--- data/data.js
+++ data/data.js
@@ -1,5 +1,15 @@
 export const data = [
   {
+    name: "336",
+    podlink: "",
+    usalink: "",
+    aliexpress: "",
+    imglink:
+      "//ae01.alicdn.com/kf/S7c258537b7004b67b5230724c1834963Q.jpg_350x350.jpg",
+    shortlink: "",
+    customlink: "https://hutaw.com/products/4pcs-wooden-tree-design-coasters"
+  },
+  {
     name: "335",
     podlink: "",
     usalink: "",
