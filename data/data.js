60cec4a2e77087e28401e515a42f7274adc122c9
Whutai <taiphantmt@gmail.com>
2024-08-25 15:07:16 +0700

    update

--- data/data.js
+++ data/data.js
@@ -1,5 +1,15 @@
 export const data = [
   {
+    name: "352",
+    podlink: "",
+    usalink: "",
+    aliexpress: "",
+    imglink:
+      "//ae01.alicdn.com/kf/S333c9cf7791a4e4f8a30a89b5f589b7bV.jpg_350x350.jpg",
+    shortlink: "",
+    customlink: "https://hutaw.com/products/portable-usb-automatic-juicer"
+  },
+  {
     name: "351",
     podlink: "",
     usalink: "",
