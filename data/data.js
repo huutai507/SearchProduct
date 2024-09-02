576880c721f7e08fbbf86b8eec96991d9dcd4392
Whutai <taiphantmt@gmail.com>
2024-09-02 20:11:31 +0700

    update

--- data/data.js
+++ data/data.js
@@ -1,5 +1,15 @@
 export const data = [
   {
+    name: "358",
+    podlink: "",
+    usalink: "",
+    aliexpress: "",
+    imglink:
+      "//ae01.alicdn.com/kf/S02ba0e588fb24bf5ad38a782e67e7d70h.jpg_350x350.jpg",
+    shortlink: "",
+    customlink: "https://hutaw.com/products/diy-garden-sprayer-kit-20m"
+  },
+  {
     name: "357",
     podlink: "",
     usalink: "",
