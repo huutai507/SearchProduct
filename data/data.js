5a47d06b2c9b3c023dadb8fb392b527fd0e3093b
Whutai <taiphantmt@gmail.com>
2024-09-30 16:26:29 +0700

    update

--- data/data.js
+++ data/data.js
@@ -1,5 +1,15 @@
 export const data = [
   {
+    name: "383",
+    podlink: "",
+    usalink: "",
+    aliexpress: "",
+    imglink:
+      "//ae01.alicdn.com/kf/S511655426bd4419bb24f20e3f594471cg.jpg_350x350.jpg",
+    shortlink: "",
+    customlink: "https://hutaw.com/products/3-speed-pressurized-faucet-extender"
+  },
+  {
     name: "382",
     podlink: "",
     usalink: "",
