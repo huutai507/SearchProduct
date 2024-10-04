c5618fbe6895766bf168f2bbe231a2a8cd29bfe9
Whutai <taiphantmt@gmail.com>
2024-10-04 20:27:14 +0700

    update

--- data/data.js
+++ data/data.js
@@ -1,5 +1,16 @@
 export const data = [
   {
+    name: "386",
+    podlink: "",
+    usalink: "",
+    aliexpress: "",
+    imglink:
+      "//ae01.alicdn.com/kf/Sdb694208aeca4cb49278dc0e89b5351bf.jpg_350x350.jpg",
+    shortlink: "",
+    customlink:
+      "https://hutaw.com/products/3-modles-adjustment-high-pressure-water"
+  },
+  {
     name: "385",
     podlink: "",
     usalink: "",
