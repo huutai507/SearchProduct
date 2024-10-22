64a2ab1e30a41a5dd3e7cc538313fb163b379e93
Whutai <taiphantmt@gmail.com>
2024-10-22 19:36:26 +0700

    update

--- data/data.js
+++ data/data.js
@@ -1,5 +1,15 @@
 export const data = [
   {
+    name: "409",
+    podlink: "",
+    usalink: "",
+    aliexpress: "",
+    imglink:
+      "//ae01.alicdn.com/kf/S27bf8a74b1b7420398f6295ac8e8fef0q.jpg_350x350.jpg",
+    shortlink: "",
+    customlink: "https://hutaw.com/products/fruit-drain-basket-with-lid"
+  },
+  {
     name: "408",
     podlink: "",
     usalink: "",
