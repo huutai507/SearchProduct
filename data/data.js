c137134a542aace1551d8f7d9c20c234d4ff3575
Whutai <taiphantmt@gmail.com>
2024-10-24 14:17:37 +0700

    update

--- data/data.js
+++ data/data.js
@@ -1,5 +1,15 @@
 export const data = [
   {
+    name: "412",
+    podlink: "",
+    usalink: "",
+    aliexpress: "",
+    imglink:
+      "//ae01.alicdn.com/kf/Scf13c48182704ee985e3e8f3fcc7fc87m.jpg_350x350.jpg",
+    shortlink: "",
+    customlink: "https://hutaw.com/products/cute-cat-plush-one-shoulder-bag"
+  },
+  {
     name: "411",
     podlink: "",
     usalink: "",
