73268a222ec0431169a47791bad7e0c1021304f4
Whutai <taiphantmt@gmail.com>
2024-11-01 19:21:12 +0700

    update

--- data/data.js
+++ data/data.js
@@ -1,5 +1,16 @@
 export const data = [
   {
+    name: "422",
+    podlink: "",
+    usalink: "",
+    aliexpress: "",
+    imglink:
+      "//ae01.alicdn.com/kf/S7de20e067c9741bbb171e3729d1a46adk.jpg_350x350.jpg",
+    shortlink: "",
+    customlink:
+      "https://hutaw.com/products/creative-automatic-page-turning-clock"
+  },
+  {
     name: "421",
     podlink: "",
     usalink: "",
