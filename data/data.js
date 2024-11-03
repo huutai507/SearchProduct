68512773af9e48c4db64d5d4bc3b00fe2849c505
Whutai <taiphantmt@gmail.com>
2024-11-03 19:33:23 +0700

    update

--- data/data.js
+++ data/data.js
@@ -1,5 +1,15 @@
 export const data = [
   {
+    name: "428",
+    podlink: "",
+    usalink: "",
+    aliexpress: "",
+    imglink:
+      "//ae01.alicdn.com/kf/H1a974dbce3a24b518c0a6bd469d8fd0da.jpg_350x350.jpg",
+    shortlink: "",
+    customlink: "https://hutaw.com/products/faucet-external-set-shower"
+  },
+  {
     name: "427",
     podlink: "",
     usalink: "",
