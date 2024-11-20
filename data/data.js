7e6c9636c3680c6c34c07d5cb2910fb5688c201e
Whutai <taiphantmt@gmail.com>
2024-11-20 08:36:04 +0700

    update

--- data/data.js
+++ data/data.js
@@ -1,5 +1,26 @@
 export const data = [
   {
+    name: "456",
+    podlink: "",
+    usalink: "",
+    aliexpress: "",
+    imglink:
+      "//ae01.alicdn.com/kf/Se60bac2d087e4ea2911cd368ed1992c6G.jpg_350x350.jpg",
+    shortlink: "",
+    customlink: "https://hutaw.com/products/home-cinema-mini-projetor"
+  },
+  {
+    name: "455",
+    podlink: "",
+    usalink: "",
+    aliexpress: "",
+    imglink:
+      "https://ae01.alicdn.com/kf/S893d629baee142f79f35b24f70da8209N/Wireless-Mattress-Vacuum-Cleaner-Cordless-Handheld-UV-C-Bed-Dust-Remover-Indepth-Cleaning-Sofa-Specialist-12Kpa.jpg_350x350.jpg_.webp",
+    shortlink: "",
+    gif: "vacuum.webp",
+    customlink: "https://hutaw.com/products/wireless-mitter-division-artifact"
+  },
+  {
     name: "454",
     podlink: "",
     usalink: "",
