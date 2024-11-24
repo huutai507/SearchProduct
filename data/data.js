56698461876f44e4d748a1bbd1144a6d07531507
Whutai <taiphantmt@gmail.com>
2024-11-24 19:28:27 +0700

    update

--- data/data.js
+++ data/data.js
@@ -1,5 +1,15 @@
 export const data = [
   {
+    name: "465",
+    podlink: "",
+    usalink: "",
+    aliexpress: "",
+    imglink:
+      "//ae01.alicdn.com/kf/Sfd4042d49ffd464789b796540a2a87719.jpg_350x350.jpg",
+    shortlink: "",
+    customlink: "https://hutaw.com/products/high-pressure-3-mode-shower-head"
+  },
+  {
     name: "464",
     podlink: "",
     usalink: "",
