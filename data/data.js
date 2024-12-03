6366ef4ca2349afed83f8c59a399218f7a05c497
Whutai <taiphantmt@gmail.com>
2024-12-03 15:24:45 +0700

    update

--- data/data.js
+++ data/data.js
@@ -1,5 +1,16 @@
 export const data = [
   {
+    name: "480",
+    podlink: "",
+    usalink: "",
+    aliexpress: "",
+    imglink:
+      "//ae01.alicdn.com/kf/S3f287d6469f640f1abcb193cc0a2f762l.jpg_350x350.jpg",
+    shortlink: "",
+    customlink:
+      "https://hutaw.com/products/magic-collapsible-folding-cat-scratch-board-lounge-bed"
+  },
+  {
     name: "479",
     podlink: "",
     usalink: "",
