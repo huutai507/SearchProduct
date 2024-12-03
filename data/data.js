b99c2881b934ad8779c78922d7c15a8d457bb781
Whutai <taiphantmt@gmail.com>
2024-12-03 21:58:18 +0700

    update

--- data/data.js
+++ data/data.js
@@ -1,5 +1,15 @@
 export const data = [
   {
+    name: "481",
+    podlink: "",
+    usalink: "",
+    aliexpress: "",
+    imglink:
+      "//ae01.alicdn.com/kf/S7d091d83010b46649efa39013d006b4aM.jpg_350x350.jpg",
+    shortlink: "",
+    customlink: "https://hutaw.com/products/christmas-tree-insulation-mat"
+  },
+  {
     name: "480",
     podlink: "",
     usalink: "",
