a8577c9ce0a62c958a5dcafe14bd812f305213e8
Whutai <taiphantmt@gmail.com>
2025-04-11 09:28:15 +0700

    update

--- data/data.js
+++ data/data.js
@@ -1,5 +1,16 @@
 export const data = [
   {
+    name: "600",
+    podlink: "",
+    usalink: "",
+    aliexpress: "",
+    imglink:
+      "https://ae-pic-a1.aliexpress-media.com/kf/S81d9abea41f24d8991c0cb8ff7b23b784.png_350x350.png_.webp",
+    shortlink: "",
+    customlink:
+      "https://hutaw.com/products/multifunctional-prone-sleeping-pillow"
+  },
+  {
     name: "599",
     podlink: "",
     usalink: "",
