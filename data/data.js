5aad369aa25cc27d48ba1de6b6111af24effe0cc
Whutai <taiphantmt@gmail.com>
2025-04-25 09:33:03 +0700

    update

--- data/data.js
+++ data/data.js
@@ -1,5 +1,15 @@
 export const data = [
   {
+    name: "617",
+    podlink: "",
+    usalink: "",
+    aliexpress: "",
+    imglink:
+      "https://ae01.alicdn.com/kf/S893d629baee142f79f35b24f70da8209N/Wireless-Mattress-Vacuum-Cleaner-Cordless-Handheld-UV-C-Bed-Dust-Remover-Indepth-Cleaning-Sofa-Specialist-12Kpa.jpg_350x350.jpg_.webp",
+    shortlink: "",
+    customlink: "https://hutaw.com/products/wireless-mitter-division-artifact"
+  },
+  {
     name: "616",
     podlink: "",
     usalink: "",
@@ -673,7 +683,6 @@ export const data = [
     imglink:
       "https://ae01.alicdn.com/kf/S893d629baee142f79f35b24f70da8209N/Wireless-Mattress-Vacuum-Cleaner-Cordless-Handheld-UV-C-Bed-Dust-Remover-Indepth-Cleaning-Sofa-Specialist-12Kpa.jpg_350x350.jpg_.webp",
     shortlink: "",
-    gif: "vacuum.webp",
     customlink: "https://hutaw.com/products/wireless-mitter-division-artifact"
   },
   {
