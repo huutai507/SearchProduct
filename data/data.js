755e50eeccf9c5a3e4c8914311ff13ef7a51f523
Whutai <taiphantmt@gmail.com>
2025-05-07 09:48:22 +0700

    update

--- data/data.js
+++ data/data.js
@@ -1,5 +1,15 @@
 export const data = [
   {
+    name: "632",
+    podlink: "",
+    usalink: "",
+    aliexpress: "",
+    imglink:
+      "//ae01.alicdn.com/kf/S3442f061dcd54910ad4966d535c8a269D.jpg_350x350.jpg",
+    shortlink: "",
+    customlink: "https://hutaw.com/products/car-seat-gap-plug-cover"
+  },
+  {
     name: "631",
     podlink: "",
     usalink: "",
