49caacca8fdd43d600f35628d79844a792daabb2
Whutai <taiphantmt@gmail.com>
2025-06-12 09:48:06 +0700

    update

--- data/data.js
+++ data/data.js
@@ -1,5 +1,15 @@
 export const data = [
   {
+    name: "658",
+    podlink: "",
+    usalink: "",
+    aliexpress: "",
+    imglink:
+      "//ae01.alicdn.com/kf/S83a3ad20ead44a498c1ba191af5c1e45p.jpg_350x350.jpg",
+    shortlink: "",
+    customlink: "https://s.click.aliexpress.com/e/_oDeHauE"
+  },
+  {
     name: "657",
     podlink: "",
     usalink: "",
