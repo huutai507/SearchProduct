146b437807238ac098cfe5c94567ef09bb7948a7
Whutai <taiphantmt@gmail.com>
2025-07-11 10:52:38 +0700

    update

--- data/data.js
+++ data/data.js
@@ -1,5 +1,15 @@
 export const data = [
   {
+    name: "694",
+    podlink: "",
+    usalink: "",
+    aliexpress: "",
+    imglink:
+      "//ae01.alicdn.com/kf/Sc058d7dc5624413d9c5043e955fa166fP.jpg_350x350.jpg",
+    shortlink: "",
+    customlink: "https://hutaw.com/products/smart-shoulder-and-neck-massager"
+  },
+  {
     name: "693",
     podlink: "",
     usalink: "",
