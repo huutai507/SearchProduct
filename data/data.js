1243b9317da563761bd360b56f2f3262a3773a63
Whutai <taiphantmt@gmail.com>
2025-10-04 05:44:19 +0700

    update

--- data/data.js
+++ data/data.js
@@ -1,5 +1,15 @@
 export const data = [
   {
+    name: "784",
+    podlink: "",
+    usalink: "",
+    aliexpress: "",
+    imglink:
+      "//ae01.alicdn.com/kf/S05501c2d2444458ca76ea03e008cd7aai.jpg_350x350.jpg",
+    shortlink: "",
+    customlink: "https://hutaw.com/products/42-head-muscle-massage-gun"
+  },
+  {
     name: "783",
     podlink: "",
     usalink: "",
