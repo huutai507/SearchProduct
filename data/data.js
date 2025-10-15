4715290187827cd03341135f029725bdac262501
Whutai <taiphantmt@gmail.com>
2025-10-15 10:37:03 +0700

    update

--- data/data.js
+++ data/data.js
@@ -1,5 +1,15 @@
 export const data = [
   {
+    name: "796",
+    podlink: "",
+    usalink: "",
+    aliexpress: "",
+    imglink:
+      "//ae01.alicdn.com/kf/Scdc9c6f05dc247a581aa5bbc1b2ca38dQ.jpg_350x350.jpg",
+    shortlink: "",
+    customlink: "https://hutaw.com/products/mini-folding-handheld-sponge-mop"
+  },
+  {
     name: "795",
     podlink: "",
     usalink: "",
